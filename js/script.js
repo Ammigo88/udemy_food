9651c8bbf10cc62babc5401579b25e94e16c67ae
Stas <s.akimov1988@yandex.ru>
2022-03-20 12:23:14 +0300

    lesson_43_final

--- js/script.js
+++ js/script.js
@@ -54,22 +54,22 @@ window.addEventListener('DOMContentLoaded', () => {
     //    вычислим время, которое установелно у пользователя и найти разницу, которую будем отображать на экране
     // 3. функция, которая будет заниматься обновлением нашего таймера
 
-    const deadline = '2022-03-17';
+    const deadline = '2022-03-20';
 
     // разница между дедлайном и нашем текущем временем
     function getTimRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              // округляем до ближайшего целого
-              // 1000 мс * 60 (получаем кол-во мс в минуте) * 60 (получаем кол-во мс в часе) * 24 (получаем кол-во мс в сутках)
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            // округляем до ближайшего целого
+            // 1000 мс * 60 (получаем кол-во мс в минуте) * 60 (получаем кол-во мс в часе) * 24 (получаем кол-во мс в сутках)
 
-              // получаем общее кол-во часов, которое осталось в таймере
-              hours = Math.floor((t / (1000 * 60 *60) % 24)),
-              
-              // минуты
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              // секунды
-              seconds = Math.floor((t / 1000) % 60);
+            // получаем общее кол-во часов, которое осталось в таймере
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+
+            // минуты
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            // секунды
+            seconds = Math.floor((t / 1000) % 60);
 
         // чтобы вернуть переменные наружу (локально)
         // возвращаем объект
@@ -85,18 +85,22 @@ window.addEventListener('DOMContentLoaded', () => {
     // функция устанавливающая наши часы на страничку
     function setClock(selector, endtime) {
         const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              // для запуска функции updateClock каждую секунду
-              timeInterval = setInterval(updateClock, 1000);
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            // для запуска функции updateClock каждую секунду
+            timeInterval = setInterval(updateClock, 1000),
+
+            promTitle = document.querySelector('#promtitle'),
+            promTimer = document.querySelector('.timer');
 
         // запускаем функцию, чтобы не было мерцания/моргания счётчика
         updateClock();
         // функция, обновляющая таймер каждую секунду
         function updateClock() {
-            const t = getTimRemaining(endtime);
+            let t = getTimRemaining(endtime);
+
 
             days.innerHTML = t.days;
             hours.innerHTML = t.hours;
@@ -106,7 +110,14 @@ window.addEventListener('DOMContentLoaded', () => {
             // если время вышло, то наш таймер не обновляем
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                // promTimer.style.display = 'none';
+                // promTitle.textContent = 'Пока акций нет';
+                // days.innerHTML = 0;
+                // hours.innerHTML = 0;
+                // minutes.innerHTML = 0;
+                // seconds.innerHTML = 0;
             }
+
         }
     }
 
@@ -114,8 +125,64 @@ window.addEventListener('DOMContentLoaded', () => {
     // устанавливаем наши часы и проверяем
     setClock('.timer', deadline);
 
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
 
 
+    console.log(modal);
+
+    modalTrigger.forEach(btn => {
+
+        btn.addEventListener('click', () => {
+        // modal.classList.add('show');
+        // modal.classList.remove('hide');
+    
+        // !!! можно через toggle
+        modal.classList.toggle('show');
+
+        // убираем прокрутку сайта, пока модальное окно открыто
+        document.body.style.overflow = 'hidden';
+        });
+    });
+
+    // создаём функцию закрытия модального окна
+    function closeModal () {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    // переписываем код ниже и используем функцию closeModal
+    // modalCloseBtn.addEventListener('click', () => {
+    //     // modal.classList.add('hide');
+    //     // modal.classList.remove('show');
+
+    //     // !!! можно через toggle
+    //     modal.classList.toggle('show');
+    //     // возвращаем прокрутку сайта, когда модальное окно закрыто
+    //     document.body.style.overflow = '';
+    // });
+
+    // скрываем модальное окно при клике на область вне этого окна
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            // modal.classList.toggle('show');
+            // document.body.style.overflow = '';
+
+            // просто добавляем ранее написанную функцию
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 
 
 });
